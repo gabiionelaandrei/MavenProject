9a1c3b0af13cb7b8b18f35e1b19816325962d06d
Andrei Gabi Ionela <45040124+gabiionelaandrei@users.noreply.github.com>
2023-06-23 16:16:59 +0300

    'curs13'

---
+++ src/main/java/curs13/GitFilteringLog.java
@@ -0,0 +1,24 @@
+package curs13;
+
+public class GitFilteringLog {
+
+	
+	
+	//Filtering the log
+	
+	// git log  = show all repo history
+	// git log --online =full log truncated to one line
+	// git log --online --all = full log history truncated and all on branches
+	//
+	
+	
+	//git log --oneline -2 = filter by number
+	//git log --oneline --after='2023-06-01'
+	//git log --oneline --before='2023-06-01'
+	
+	//git log --author='Dragos'
+	//git log --online --grep='final'
+	
+	
+	
+}

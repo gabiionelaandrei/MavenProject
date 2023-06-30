6272fb994b05fe2b5927bae3aae2abbea7ccbcde
Andrei Gabi Ionela <45040124+gabiionelaandrei@users.noreply.github.com>
2023-06-30 11:20:01 +0300

    Add files via upload

---
+++ curs12/TestJsonProcessor.java
@@ -0,0 +1,18 @@
+package curs12;
+
+import org.json.simple.parser.ParseException;
+
+public class TestJsonProcessor {
+
+	public static void main(String[] args) throws ParseException {
+
+		JsonProcessor obj = new JsonProcessor();
+		obj.writeJson();
+		obj.readJson("email");
+		obj.readJson("listaObiecte");
+		obj.updateJsonFile("email", "altEmail");
+		obj.deleteFromJsonFile("email");
+		
+	}
+
+}

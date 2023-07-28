069de9fbafe02d4bbb5b99bddfae2feb214e8dfe
Andrei Gabi Ionela <45040124+gabiionelaandrei@users.noreply.github.com>
2023-07-28 15:22:45 +0300

    curs16

--- src/main/java/curs16/IsSelectedExample.java
+++ src/main/java/curs16/IsSelectedExample.java
@@ -6,25 +6,32 @@ import org.testng.annotations.Test;
 
 import utils.BaseTest;
 
-public class IsSelectedExample extends BaseTest {
+public class IsSelectedExample extends BaseTest{
+
 	
 	@Test
-	
-	public void isSelectedTest() {
+	public void isSelectedTest() throws InterruptedException {
 		
-	driver.findElement(By.cssSelector("li[class='menu_user_login']>a[href*='popup_login']")).click();
+		driver.findElement(By.cssSelector("li[class='menu_user_login']>a[href*='popup_login']")).click();
 		
+		WebElement username = driver.findElement(By.cssSelector("div[class='form_left'] input[id='log']"));
+		
+		System.out.println(username.isSelected());	
+		username.click();
+		System.out.println("After click :" + username.isSelected());
+		
+		System.out.println("-------------------------------------------");
+		Thread.sleep(4000);
+		WebElement rememberMe =  driver.findElement(By.cssSelector("input[id='rememberme']"));
+		
+		System.out.println("Before click :" + rememberMe.isSelected());
+		rememberMe.click();
+		System.out.println("After click :" + rememberMe.isSelected());
 
-	WebElement username = driver.findElement(By.cssSelector("div[class='form_left'] input[id='log']"));
-	System.out.println(username.isSelected());
-	username.click();
-	System.out.println("After click " + username.isSelected());
-	System.out.println("-------------------------");
-
-	WebElement rememberMe = driver.findElement(By.cssSelector("li[class='menu_user_login'] input[id='rememberme']"));
-	System.out.println("Before click " + rememberMe.isSelected());
-	rememberMe.click();
-	System.out.println("After click " + rememberMe.isSelected());
+		
+		
+		
 	}
-
+	
+	
 }
